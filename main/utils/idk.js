f5744747e062a3ade707a673d553de120b2d973d
Boter <145221959+AButterLamp@users.noreply.github.com>
2024-10-31 16:05:13 +0000

    Update idk.js

--- main/utils/idk.js
+++ main/utils/idk.js
@@ -1,6 +1,8 @@
 import * as base from "https://static.sparxhomework.uk/sw2/b5647ce1effb454936bedd0ba967735a639e5c5b/assets/index-yALBpciV.js";
 console.log("Neww update!")
 
-for (const prop in base){
+/*for (const prop in base){
   console.log(base[prop])
-}
+}*/
+
+console.log(c.j)
